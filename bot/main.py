e16d9d76627a14c34edcc641d7476147e44ebce0
Caner <canerturkmen@msn.com>
2021-04-24 02:21:26 +0300

    emoji added

--- bot/main.py
+++ bot/main.py
@@ -8,8 +8,12 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 async def on_message(message):
 
     if message.content.startswith('31'):
-      await message.channel.send("hahahaha")
-      return 
+        await message.channel.send("hahahaha")
+        return 
+    
+    if message.content.startswith("mestan") or message.content.startswith("Mestan") or message.content.startswith("MESTAN"):
+        await message.channel.send("mestan açılma artık")
+        return 
     c = 0
     for x in message.content:
         c += 1
@@ -24,36 +28,45 @@ async def on_message(message):
             a = message.content[0:c-1]
             b = message.content[c:]
             if int(a) - int(b) == 31:
-                await message.channel.send("cok iyi saka kanka ama eksilisi")
+                await message.channel.send("cok iyi saka kanka valla")
                 return
 
         elif x == "*":
             a = message.content[0:c-1]
             b = message.content[c:]
             if int(a) * int(b) == 31:
-                await message.channel.send("cok iyi saka kanka ama carpili")
+                await message.channel.send("cok iyi saka kanka güzel çarpma tebrik ederim")
                 return
 
         elif x == "/":
             a = message.content[0:c-1]
             b = message.content[c:]
             if int(a) / int(b) == 31:
-                await message.channel.send("cok iyi saka kanka ama bolulu")
+                await message.channel.send("cok iyi saka kanka güzel bölme işlemi tebrik ederim")
                 return
 
         elif x == ":":
             a = message.content[0:c-1]
             b = message.content[c:]
             if int(a) / int(b) == 31:
-                await message.channel.send("cok iyi saka kanka ama bolulu")
+                await message.channel.send("cok iyi saka kanka en son bakkalda gördüm : bu işareti ")
                 return
 
     if "31" in message.content:
         await message.channel.send("gizli saka")
         return
 
-    elif message.content.startswith("5v5"):
-        await message.channel.send("ben oynarim kanka")
+    if message.content.startswith("5v5"):
+        await message.channel.send("ben oynarim kanka yaz beni")
+        await message.channel.send(discord.emoji("\:laughing:"))
         return 
-        
+    
+    if message.content.startswith("gülemem"):
+        await message.channel.send("gülemem kanka gülemem s'nin yanına j gelmeyince gülemem")
+        return
+    
+    if message.content.startswith("helikopter"):
+        await message.channel.send("pat pat")
+        return
+
 client.run(token)
